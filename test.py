c107eaa758b16df184905c877b7045f40247f3af
Aaron Huang <ahuang25@andover.edu>
2024-09-25 19:31:46 -0400

    Added Features

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-stu = 9
+stu = 93
+popo = 2
+print("i love soccer")
+
+# shaded blue means line is changed
+# red arrow means line is deleted
+# green means new line
+# all in comparison to the previous version of the file
